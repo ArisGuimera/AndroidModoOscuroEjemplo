9bb6c65e57c93ae80af78e460f23544c40ce75e1
AristiDevs <aris.guimera@gmail.com>
2023-02-24 11:36:43 +0000

    initial commit

---
+++ app/src/main/java/com/aristidevs/darkmodeexample/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.aristidevs.darkmodeexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import com.google.android.material.switchmaterial.SwitchMaterial
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val swDarkMode = findViewById<SwitchMaterial>(R.id.swDarkMode)
+
+        swDarkMode.setOnCheckedChangeListener { _, isSelected ->
+            if (isSelected){
+                enableDarkMode()
+            }else{
+                disableDarkMode()
+            }
+        }
+    }
+
+    private fun enableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        delegate.applyDayNight()
+    }
+
+    private fun disableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+        delegate.applyDayNight()
+    }
+}
